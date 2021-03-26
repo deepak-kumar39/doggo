cda3fafd2b03876f7c68eaea70718956c74cc501
deepak-kumar39 <deepakkumarrr39@gmail.com>
2021-03-26 14:23:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,50 +4,53 @@ import React, { useEffect, useState } from "react";
 export default function App() {
     const [breed, setBreed] = useState([]);
     const [val, setVal] = useState("");
-    const [src, setSrc] = useState("https://images.dog.ceo/breeds/affenpinscher/n02110627_2157.jpg");
+    const [src, setSrc] = useState(
+        "https://images.dog.ceo/breeds/affenpinscher/n02110627_2157.jpg"
+    );
     const [err, setErr] = useState("");
 
     useEffect(() => {
         fetch("https://dog.ceo/api/breeds/list/all")
-          .then((response) => response.json())
-          .then((data) => setBreed(Object.entries(data.message)));
+            .then((response) => response.json())
+            .then((data) => setBreed(Object.entries(data.message)));
         breed &&
-          setVal(
-            breed.map((item, index) =>
-              item[1].length > 0 ? (
-                item[1].map((itm) => <option>{item[0] + " " + itm}</option>)
-              ) : (
-                <option>{item[0]}</option>
-              )
-            )
-          );
-      }, [breed]);
+            setVal(
+                breed.map((item, index) =>
+                    item[1].length > 0 ? (
+                        item[1].map((itm, idx) => (
+                            <option key={idx}>{item[0] + " " + itm}</option>
+                        ))
+                    ) : (
+                        <option key={index}>{item[0]}</option>
+                    )
+                )
+            );
+    }, [breed]);
     const handleSelect = (event) => {
         let val = event.target.value.split(" ");
-    let url =
-      val.length > 1
-        ? `https://dog.ceo/api/breed/${val[0]}/${val[1]}/images/random`
-        : `https://dog.ceo/api/breed/${val[0]}/images/random`;
+        let url =
+            val.length > 1
+                ? `https://dog.ceo/api/breed/${val[0]}/${val[1]}/images/random`
+                : `https://dog.ceo/api/breed/${val[0]}/images/random`;
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setSrc(data.message))
-      .catch(() => setErr("Breed Not Found"));
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => setSrc(data.message))
+            .catch(() => setErr("Breed Not Found"));
         // console.log(src);
     };
     return (
         <div className="App">
             <h1>Select Breed from the list</h1>
-            <select className="box" onClick={handleSelect}>{val}</select>
+            <select className="box" onClick={handleSelect}>
+                {val}
+            </select>
             {src ? (
-        <div>
-              {err ? err: <img src={src} alt="dog" />}
-        </div>
-      ) : (
-        <h3> No image to show</h3>
-      )}
+                <div>{err ? err : <img src={src} alt="dog" />}</div>
+            ) : (
+                <h3> No image to show</h3>
+            )}
             <div className="container">{src.split("/")[4]}</div>
         </div>
     );
 }
-
